85d9013b4c058ea88b56e543bdf360c4a03713fd
Woodsandfields <k.gantin.kg@gmail.com>
2018-10-08 13:07:08 +0200

    adding figures on map itself etc.

--- FrScholMapScript.R
+++ FrScholMapScript.R
@@ -23,8 +23,8 @@ theRegions <- st_read(region_location)
 ## This gives a 3-column dataframe (as a subset from the original shp(sf)-dataframe, automatically includes geometry)
 
 myRegions <- tbl_df(theRegions) %>%
-        select(NAME, SUBREGION, geometry) %>%
-        mutate(SUBREGION=as.factor(SUBREGION), NAME=as.character(NAME)) 
+        select(NAME, SUBREGION, LON, LAT, geometry) %>%
+        mutate(SUBREGION=as.factor(SUBREGION), NAME=as.character(NAME))
 
 
 
@@ -41,7 +41,7 @@ OrderedList <- myRegions[order(myRegions$SUBREGION),]
 #add a column
 
 myRegions <- mutate(myRegions, Regions=" ")
-myRegions$Regions[myRegions$Regions =="0"] <- "Antarctica"
+myRegions$Regions[myRegions$SUBREGION =="0"] <- "Antarctica"
 
 myRegions$Regions[myRegions$SUBREGION %in% c(39, 151, 154, 155)] <- "Europe"
 myRegions$Regions[myRegions$SUBREGION %in% c(11, 14, 17, 18)] <- "Sub-Saharan Africa"
@@ -61,28 +61,67 @@ myRegions$schol[myRegions$schol == "Maghreb"]<- 2897
 myRegions$schol[myRegions$schol == "Sub-Saharan Africa"]<- 2906
 myRegions$schol[myRegions$schol == "Asia & Oceania"]<- 2662
 myRegions$schol[myRegions$schol == "America"]<- 1770
-myRegions$schol[myRegions$schol == "Antarctica"]<- 0
+myRegions$schol[myRegions$schol == "Antarctica"]<- NA
 
 myRegions$schol <- as.numeric(myRegions$schol)
 
+# Substituting the regional mean for LON and LAT,
+# & Correcting LON/LAT for Asia & Oceania and Antartica Regions 
+# to have them more conveniently placed on the map.
+
+myRegions_notRegrouped <- myRegions
+myRegions <- myRegions %>%
+        group_by(Regions) %>%
+        mutate(LON = mean(LON), LAT = mean(LAT),
+               LON = case_when(Regions == "Asia & Oceania" ~ LON+50, TRUE  ~ LON),
+               LAT = case_when(Regions == "Antarctica" ~ LAT-50, TRUE ~ LAT))
+
+
+
+
+### MAKING THE MAP
+
+# myColors <- brewer.pal(12,"Set3") 
+# names(myColors) <- levels(as.factor(myRegions$Regions))
+# #colScale <- scale_color_manual(name = "SUBREGION",values = myColors)
+
+
+
+# Here is the choropeth map as expected in the final document to produce for the Knight Foundation.
+# However, it doesn't look good and add less than the format we will try as a next step.
+# So we'll try something else for the final presentation and produce two maps instead of one.
+
+myRegions$schol <- as.numeric(myRegions$schol)
 
 myColors <- brewer.pal(12,"Set3") 
 names(myColors) <- levels(as.factor(myRegions$Regions))
 #colScale <- scale_color_manual(name = "SUBREGION",values = myColors)
 
 myMap <- ggplot(myRegions) + geom_sf(aes(fill=schol,color = Regions))+
-        scale_fill_distiller(direction = 1, name =" Number of Scholarships", palette="Oranges")+
-        labs(caption="Number of Scholarships Given in 2011 by World Region", subtitle="Scholarships to Foreign Students by the French Government")
+        scale_fill_distiller(direction = 1, 
+                             name =" Number of Scholarships", palette="Oranges")+
+        labs(caption="Number of Scholarships Given by World Region in 2016", 
+             subtitle="Scholarships to Foreign Students by the French Government")+
+        geom_label(aes(x=LON, y=LAT), 
+                   color="black", fill = "grey", 
+                   label=myRegions$Regions, size=2)+
+        xlab("") + ylab("") +
+        guides(color = FALSE)
 
-print(myMap)
+# print(myMap)
 
-# It is the expected chloropeth map. However, it doesn't look good.
-# So we try something else for the final presentation.
+# Here is another, more satisfying map.
 
-myMap2 <- ggplot(myRegions) + geom_sf(aes(fill = Regions))+
-        labs(caption="Number of Scholarships Given in 2011 by World Region", subtitle="Scholarships to Foreign Students by the French Government")
+myMap2 <- ggplot(myRegions) + geom_sf(aes(fill=Regions,color = Regions))+
+        scale_fill_brewer(palette="Set1")+
+        labs(caption="Number of Scholarships Given by World Region in 2016", 
+             title="World Repartition of French Government Scholarships")+
+        geom_label(aes(x=LON, y=LAT), 
+                   color="black", fill = "grey", 
+                   label=myRegions$schol, size=2.2)+
+        xlab("") + ylab("")
 
-# print(myMap2)
+print(myMap2)
 
 ggsave('myMap2.png', myMap2, width = 16, height = 9, dpi = 100)
 ggsave('myMap1.png', myMap, width = 16, height = 9, dpi = 100)
