6191d1705a402841e7751eff6f3a1d30bf3b5fb2
Woodsandfields <k.gantin.kg@gmail.com>
2018-10-24 10:58:01 +0200

    more data tidying with tidyr::gather

--- Script_for_the_plot.R
+++ Script_for_the_plot.R
@@ -1,6 +1,6 @@
 
 
-packages <- c("R.utils", "here", "data.table", "readr", "ggplot2", "dplyr")
+packages <- c("R.utils", "here", "data.table", "readr", "ggplot2", "dplyr", "tidyr")
 
 for (package in packages) {
         if (!require(package, character.only=T, quietly=T)) {
@@ -29,10 +29,19 @@ if (!is.null(WD)) setwd(WD)
 dataUrl <- 'http://www.data.gouv.fr/fr/datasets/r/dd3b4807-d90c-46fe-b03a-2ad250db979f'
 
 dir.create("originalData", showWarnings = F)
-download.file(dataUrl, "./originalData/myData.csv")
+
+# Comment / Uncomment next line (uncomment if program run for 1st time / dataset not yet downloaded)
+
+# download.file(dataUrl, "./originalData/myData.csv")
+
+
 
 globalData <- read.table("originalData/myData.csv", sep=";")
 
+# Some data exploring gives a view of what has to be done to tidy the set. 
+# As the dataset is short, a View(globalData) is any case a good option for a start.
+
+# View(globalData)
 
 #### DATA TIDYING ####
 
@@ -43,39 +52,31 @@ tidyData <- globalData[4:15,2:7] %>%
         as.data.frame(.)
 
 
-#Putting years as rows (inverting rows and columns)
-Years <- as.factor(as.character(c(2006:2011)))
+#Putting years as rows after inverting rows and columns with the t transpose function
 
-finalTidy <- as.data.frame(t(tidyData)) %>% 
+Years <- as.factor(as.character(c(2006:2011)))
+Regions <- c("Years", "UE", "Eur_hors_UE", "Europe", "Am_N", "America", "Asia/Oceania", "Middle East", "AfSubS_F", "AfSubS_nF","Sub-Saharan Africa","Maghreb", "Total_Ge")
+
+tidySet <- as.data.frame(t(tidyData)) %>% 
         cbind(Years, .)
 
-Regions2 <- as.character(globalData[4:15,1])
-Regions <- c("Years", "UE", "Eur_hors_UE", "Total_Eur", "Am_N", "Total_Am", "Total_Asie_O", "MO", "AfSubS_F", "AfSubS_nF","Total_AfSubS", "Maghreb", "Total_Ge")
-colnames(finalTidy) <- Regions
-rownames(finalTidy) <- Years
+colnames(tidySet) <- Regions
+rownames(tidySet) <- Years
 
 
-##
-data_Eur <- cbind(finalTidy[,c(1,4)], Region=rep("Europe", 6))
-data_Am <- cbind(finalTidy[,c(1,6)], Region=rep("America", 6))
-data_Asia <- cbind(finalTidy[,c(1,7)], Region=rep("Asia/OCeania", 6))
-data_ME <- cbind(finalTidy[,c(1,8)], Region=rep("Middle East", 6))
-data_Afr <- cbind(finalTidy[,c(1,11)], Region=rep("Sub-Saharan Africa", 6))
-data_Magh <- cbind(finalTidy[,c(1,12)], Region=rep("Maghreb", 6))
+tidySet <- tidySet %>% 
+        gather(Years, count) 
 
+colnames(tidySet) <- c("Years", "Region", "Total")
 
-names(data_Eur)<- c("Years", "Total", "Region")
-names(data_Am)<- c("Years", "Total", "Region")
-names(data_Asia)<- c("Years", "Total", "Region")
-names(data_ME)<- c("Years", "Total", "Region")
-names(data_Afr)<- c("Years", "Total", "Region")
-names(data_Magh)<- c("Years", "Total", "Region")
-
-long_Data <- rbind(data_Afr, data_Am, data_Asia, data_Eur, data_Magh, data_ME)
+tidySet <- tidySet %>%
+        filter(Region %in% c("Europe", "America", 
+                             "Asia/Oceania", "Middle East",
+                             "Sub-Saharan Africa", "Maghreb"))
 
 #### DATA VIZ ####
 
-ScholPlot <- ggplot(data=long_Data) + geom_point(aes(x=Years, y=Total, color = Region, size= 10)) +
+ScholPlot <- ggplot(data=tidySet) + geom_point(aes(x=Years, y=Total, color = Region, size= 10)) +
         geom_line(aes(x=Years, y=Total, group = Region)) + guides(size=FALSE)
 print(ScholPlot)
 
