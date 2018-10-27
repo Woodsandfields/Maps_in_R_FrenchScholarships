58ec8b429b7ffabade976a6b976ae47a84f38dd6
Woodsandfields <k.gantin.kg@gmail.com>
2018-10-27 15:40:51 +0200

    making 2d plot with labels instead of legend

--- Script_for_the_plot.R
+++ Script_for_the_plot.R
@@ -8,12 +8,8 @@ for (package in packages) {
         library(package, character.only=T)
 }
 
+print ("-------------------> If error by first launch, launch it again: packages you need might take long to install, throwing a temporary error.")
 
-
-
-
-
-## (ggplot) + annotate("text",label= "Lost 8 points because of bad luck!", x= "Arsenal", y=-3) +
 
 #### DATA DWNLD #### 
 
@@ -23,8 +19,6 @@ if (!is.null(WD)) setwd(WD)
 
 
 # You might want to uncomment the next three lines in order to avoid downloading again and again.
-
-
 
 dataUrl <- 'http://www.data.gouv.fr/fr/datasets/r/dd3b4807-d90c-46fe-b03a-2ad250db979f'
 
@@ -76,8 +70,33 @@ tidySet <- tidySet %>%
 
 #### DATA VIZ ####
 
-ScholPlot <- ggplot(data=tidySet) + geom_point(aes(x=Years, y=Total, color = Region, size= 10)) +
-        geom_line(aes(x=Years, y=Total, group = Region)) + guides(size=FALSE)
-print(ScholPlot)
 
-ggsave('ScholPlot.png', width = 16, height = 9, dpi = 100)
+ScholPlot <- ggplot(data=tidySet) + geom_point(aes(x=Years, y=Total, color = Region, size= 10)) +
+        geom_line(aes(x=Years, y=Total, group = Region)) + guides(size=FALSE)+ 
+        ylab("Number of Scholarships")
+
+# print(ScholPlot)
+
+
+#### integrating legend to plot with annotations####
+
+# Retrieving colors through the console
+g <- ggplot_build(ScholPlot)
+h <-unique(g$data[[1]]["colour"])
+print(h)
+
+#Matching colors with their regions
+
+ScholPlot2 <- ScholPlot + annotate("text", label="Europe", color = "#00BA38", x=2, y = 3450)+
+        annotate("text", label="America", color = "#F8766D", x=2, y = 1550)+
+        annotate("text", label="Asia/OCeania", color = "#B79F00", x=2, y = 2850)+
+        annotate("text", label="Middle East", color = "#619CFF", x=2, y = 2180)+
+        annotate("text", label="Maghreb", color = "#F564E3", 1.5, y = 3820)+
+        annotate("text", label="Sub-Saharan Africa", color = "#00BFC4", x=4, y = 3600)+
+        theme(legend.position = "none")
+        
+
+print(ScholPlot2)
+
+ggsave('ScholPlot.png', width = 16, height = 9, dpi = 100) 
+ggsave('ScholPlot2.png', width = 16, height = 9, dpi = 100) 
